ce36118325f0b3ab41ee2e9aa5276e47c0de6627
fantastas <msgitara@gmail.com>
2021-09-18 19:11:39 +0800

    belankas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'muza';
+  
+
+  audioObj = new Audio();
+  
+  files = 
+  [{
+    url: './assets/02 Like A Ghost.mp3',
+    name: 'Like A Ghost'
+  },
+  {
+    url: './assets/01 Pulling Every String.mp3',
+    name: 'Pulling Every String'
+  }
+  
+  ];
+
+  play(){
+    console.log('play');
+    this.audioObj.play();
+    
+
+  }
+
+  pause(){
+    console.log('pause');
+    this.audioObj.pause();
+
+
+
+  }
+
+  stop(){
+    console.log('stop');
+    this.audioObj.pause();
+    this.audioObj.currentTime = 0;
+
+
+
+  }
+
+  openFile(url:any){
+    this.audioObj.src = url;
+    this.audioObj.load();
+    this.audioObj.play();
+    console.log(url);
+    
+
+
+  }  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,48 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgxAudioPlayerModule } from 'ngx-audio-player';
-import { Track } from 'ngx-audio-player';   
 
 
-msaapDisplayTitle = true;
-msaapDisplayPlayList = true;
-msaapPageSizeOptions = [2,4,6];
-msaapDisplayVolumeControls = true;
-msaapDisplayRepeatControls = true;
-msaapDisplayArtist = false;
-msaapDisplayDuration = false;
-msaapDisablePositionSlider = true;
-   
-// Material Style Advance Audio Player Playlist
-msaapPlaylist: Track[] = [
-  {
-    title: 'Audio One Title',
-    link: 'Link to Audio One URL',
-    artist: 'Audio One Artist',
-    duration: 'Audio One Duration in seconds'
-  },
-  {
-    title: 'Audio Two Title',
-    link: 'Link to Audio Two URL',
-    artist: 'Audio Two Artist',
-    duration: 'Audio Two Duration in seconds'
-  },
-  {
-    title: 'Audio Three Title',
-    link: 'Link to Audio Three URL',
-    artist: 'Audio Three Artist',
-    duration: 'Audio Three Duration in seconds'
-  },
-];
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NgxAudioPlayerModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
